635404568e5642dcca66f9a5b521f7204275066e
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-09-19 23:16:58 -0300

    Project base configuration
    Direct download by start.spring.io

---
+++ src/main/java/com/mealfinder/MealFinderApi/MealFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.mealfinder.MealFinderApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealFinderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mealfinder/MealFinderApi/MealFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealfinder.MealFinderApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealFinderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
