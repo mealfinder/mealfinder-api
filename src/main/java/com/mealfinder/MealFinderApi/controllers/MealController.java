53c2ca5a72b1b9714cdadd359bc8fe2ea8b0bf4a
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-09-21 02:43:10 -0300

    Updated: CrossOrigin to *

--- src/main/java/com/mealfinder/MealFinderApi/controllers/MealController.java
+++ src/main/java/com/mealfinder/MealFinderApi/controllers/MealController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/meals")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class MealController {
